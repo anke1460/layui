f49082c0ca203838952815c6df01743d11e35937
anke1460 <zuosjob@gmail.com>
2022-07-21 16:46:49 +0800

    fix 0

--- src/modules/table.js
+++ src/modules/table.js
@@ -86,7 +86,7 @@ layui.define(['laytpl', 'laypage', 'layer', 'form', 'util'], function(exports){
     }() : content;
     if (!item3.templet && omit == undefined) {
       var str = str.toString();
-      if (isNaN(str) == false && options.decimals_length) {
+      if (isNaN(str) == false && options.decimals_length && str.length > options.decimals_length) {
         str = Number(str).toFixed(options.decimals_length+1)
         var strIndex = str.indexOf('.');
         if (strIndex > -1) {
